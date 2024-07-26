68027c897d6366e46d5cda1a4e3daf0989027bd3
kmjsatori <kmjsatori@qq.com>
2024-07-26 15:45:36 +0800

    更改了文件结构，加入了前端的文件

---
+++ vue/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style="font-size: 16px; padding: 20px 0; text-align: center; line-height: 30px; color: #666">
+    <div>京ICP备666888999 ©版权所有 知识星球@项目训练营</div>
+    <div> 公司地址：安徽省合肥市天鹅湖大剧院 | 公司座机：0551-78899887</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {}
+  },
+  created() {
+
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
